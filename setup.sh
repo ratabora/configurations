c5ed3c51925e4c4d5ed3a2f69804644bc9eb389a
rtabora <rtabora@riotgames.com>
2016-10-25 17:26:47 -0700

    added setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s ~/code/dotfiles/.workrc ~/
+ln -s ~/code/dotfiles/.screenrc ~/
+ln -s ~/code/dotfiles/.tmux.conf ~/
+ln -s ~/code/dotfiles/.localrc ~/
+ln -s ~/code/dotfiles/.vimrc ~/
+ln -s ~/code/dotfiles/.zshrc ~/
