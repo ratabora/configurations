f5766a28ac4f6fdc42983ea5025af42135f0c561
ratabora <ratabora@gmail.com>
2017-01-10 11:44:46 -0800

    adding install script for arch

---
+++ install/arch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+: "${CODE_HOME?Need to set CODE_HOME}"
+echo "Need to pre install sudo and add user to sudoers file"
+# TODO: sudo dialog wifi-menu
+
+
+# install all my wonderful packages
+sudo pacman -S --noconfirm --needed base-devel chromium vim git thefuck linux-headers zsh zsh-completions gnome-tweak-tool ntp xf86-input-synaptics
+
+# install yaourt
+cd $CODE_HOME
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si --noconfirm --needed
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si --noconfirm --needed
+cd ..
+
+yaourt -S --noconfirm --needed touchegg gnome-session-properties
+
+# update os
+sudo pacman -Syu
